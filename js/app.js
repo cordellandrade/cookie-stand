a469d6abdff7cc0450906a4dd6092e65071a204e
Cordell Andrade <cordellandrade93@gmail.com>
2021-01-27 23:42:24 -0500

    Original start on cookie-stand

---
+++ js/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+let seattle = {
+    name: 'Seattle',
+    min: 23,
+    max = 65,
+    ave: 6.5,
+    cookiesSoldPerHourArray: [],
+    dailyStoreTotal: 0,
+    randomCookiePerHour: function() {
+
+    }
+
+        calCookiesSoldPerHour: function() {
+
+    }
+};
+
+let tokyo = {
+    name: 'Tokyo',
+    min: 3,
+    max: 24,
+    avg: 1.2,
+};
+
+let dubai = {
+    name: 'Dubai',
+    min: 11,
+    max: 38,
+    avg: 3.7,
+};
+
+let paris = {
+    name: 'Paris',
+    min: 20,
+    max: 38,
+    avg: 2.3,
+
+};
+
+let lima = {
+    name: 'Lima',
+    min: 2,
+    max: 16,
+    avg: 4.6
+};
