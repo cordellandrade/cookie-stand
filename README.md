e50bc9ce3b3304de13ee5babb04beb46fad686f1
Cordell Andrade <cordellandrade93@gmail.com>
2021-02-09 19:48:11 -0500

    Added form

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Author: Cordell Andrade/201-21
 ### Links and Resources
 * [reset.css](https://meyerweb.com/eric/tools/css/reset/)
 * Leaned on Class lecture for initial start
+* Leaned on code review
 
 ### Reflections and Comments
 * I felt this lab went really well, was not that difficult to complete.
--- js/app.js
+++ js/app.js
@@ -1,61 +1,63 @@
 'use strict';
 
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-let cookieTable = document.getElementById('cookie-table');
+// Store Hours
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var cookieTable = document.getElementById('cookie-table');
+var storeForm = document.getElementById('storeForm');
+var tfoot = document.createElement('tfoot');
 
-// a minor fix should be able to get this up and running.
-// your functions inside your constructor need to be taken out of the constructor and named Store.prototype.randomCustomer etc
-// for all 3 functions.
-// i like you are being proactive with the form but
-// let 's get this lab done first.
-// your table should have a thead, tbody and tfoot elements.then you should just target those with an id to render.
+var storeArray = [];
 
-function Store(name, min, max, avg) {
-    this.name = name;
-    this.min = min;
-    this.max = max;
-    this.avg = avg;
-    this.cookiesSoldPerHourArray = [];
+// Location Objects
+function Store(location, minCust, maxCust, aveSale) {
+    this.location = location;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.aveSale = aveSale;
+    this.cookiesPurchasedPerHourArr = [];
     this.dailyStoreTotal = 0;
-}
+    storeArray.push(this);
+    this.getRanCust = function() {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+    };
+    this.cookiesPurchasedPerHour = function() {
+        for (var i = 0; i < hours.length; i++) {
+            var cookiesPerHour = Math.ceil(this.getRanCust() * this.aveSale);
+            this.cookiesPurchasedPerHourArr.push(cookiesPerHour);
+            this.dailyStoreTotal += cookiesPerHour;
 
-Store.prototype.randomCustomer = function() {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-}
-Store.prototype.cookiesPurchasedPerHour = function() {
-    for (let i = 0; i < hours.length; i++) {
-        let cookiesPerHour = Math.ceil(this.randomCustomer() * this.avg);
-        this.cookiesSoldPerHourArray.push(cookiesPerHour);
-        this.dailyStoreTotal += cookiesPerHour;
+        }
     }
-}
-Store.prototype.render = function() {
-    this.cookiesPurchasedPerHour();
-    let tr = document.createElement('tr');
-    let td = document.createElement('td');
-    td.textContent = this.name;
-    tr.appendChild(td);
-
-    for (let i = 0; i < hours.length; i++) {
-        let td = document.createElement('td');
-        td.textContent = this.cookiesSoldPerHourArray[i];
+    this.render = function() {
+        this.cookiesPurchasedPerHour();
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.textContent = this.location;
         tr.appendChild(td);
+
+        for (var i = 0; i < hours.length; i++) {
+            var td = document.createElement('td');
+            td.textContent = this.cookiesPurchasedPerHourArr[i];
+            tr.appendChild(td);
+        }
+        var td = document.createElement('td');
+        td.textContent = this.dailyStoreTotal;
+        tr.appendChild(td);
+        cookieTable.appendChild(tr);
     }
 
-    td = document.createElement('td');
-    td.textContent = this.dailyStoreTotal;
-    tr.appendChild(td);
-    cookieTable.appendChild(tr);
+
 }
-
-
 
 function renderHeader() {
-    let tr = document.createElement('tr');
-    let th = document.createElement('th');
+    var tr = document.createElement('tr');
+    cookieTable.appendChild(tr);
+
+    var th = document.createElement('th');
+    th.textContent = '';
     tr.appendChild(th);
 
-    for (let i = 0; i < hours.length; i++) {
+    for (var i = 0; i < hours.length; i++) {
         th = document.createElement('th');
         th.textContent = hours[i];
         tr.appendChild(th);
@@ -64,22 +66,69 @@ function renderHeader() {
     th = document.createElement('th');
     th.textContent = 'Total';
     tr.appendChild(th);
-
-    cookieTable.appendChild(tr);
 }
 
+function renderFooter() {
+
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    td.textContent = 'Grand Total:';
+    tr.appendChild(td);
+
+    cookieTable.appendChild(tfoot);
+
+    var grandTotal = 0;
+
+    for (var i = 0; i < hours.length; i++) {
+        var hourlyCounter = 0;
+        for (var j = 0; j < storeArray.length; j++) {
+            hourlyCounter += storeArray[j].cookiesPurchasedPerHourArr[i];
+
+        }
+        td = document.createElement('td');
+        td.textContent = hourlyCounter;
+        tr.appendChild(td);
+        grandTotal += hourlyCounter;
+
+    }
+    td = document.createElement('td');
+    td.textContent = grandTotal;
+    tr.appendChild(td);
+    tfoot.appendChild(tr);
+    cookieTable.appendChild(tfoot);
+}
+
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 2.3);
+var lima = new Store('Lima', 2, 16, 4.6);
+
+function newStore(event) {
+    event.preventDefault();
 
 
-let seattle = new Store('Seattle', 23, 65, 6.3);
-let tokyo = new Store('Tokyo', 3, 24, 1.2);
-let dubai = new Store('Dubai', 11, 38, 3.7);
-let paris = new Store('Paris', 20, 38, 2.3);
-let lima = new Store('Lima', 2, 16, 4.6);
 
+    var newMinCustPerHour = parseInt(event.target.minCustPerHour.value);
+    var newMaxCustPerHour = parseInt(event.target.maxCustPerHour.value);
+    var newAvgCust = parseInt(event.target.aveCust.value);
+    var newLocation = event.target.location.value;
+    console.log(typeof newMinCustPerHour);
+    var newStore = new Store(newLocation, newMinCustPerHour, newMaxCustPerHour, newAvgCust);
+
+    tfoot.innerHTML = '';
+    newStore.render();
+    renderFooter();
+}
 
 renderHeader();
+
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+
+renderFooter();
+
+storeForm.addEventListener('submit', newStore);
